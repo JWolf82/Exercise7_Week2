c01bd1e3338995ab908bd351bb8ddd56f5765045
JWolf82 <jlwolf82@gmail.com>
2016-04-13 17:45:13 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// function LetterCapitalize(str) { 
+
+//   var arr = str.split(" ");
+//   for (var i = 0; i < arr.length; i++) {
+//     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+//   }
+  
+//   return arr.join(" ");
+         
+// }
+
+// console.log(LetterCapitalize("this is a test"))
+
+// function wordCount(string){
+//     var array = string.split(" ")
+//     return array
+// }
+
+// console.log(wordCount("this is a test").length)
